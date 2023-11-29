6834fe57a1668a20e0088d87e9101d72fec294f0
rachel hu <rh111@wellesley.edu>
2023-11-28 21:09:15 -0500

    Co-authored-by: LilymoonW <LilymoonW@users.noreply.github.com> Co-authored-by: sophieelin <sophieelin@users.noreply.github.com>

--- HollywoodGraph.java
+++ HollywoodGraph.java
@@ -1,54 +1,144 @@
-import java.util.Vector;
-import java.util.
 
 /**
- *@author Lilymoon Whalen
-  Rachel Hu, Sophie Lin
- *@version
+ * Creates a graph representing the relationship between various omvies and actors based on an input file
+ * 
+ * @author Sophie Lin
+ * @author Rachel Hu
+ * @author Lilymoon Whalen
+ * @version November 28, 2023
  */
 
- public class HollywoodGraph implements Graph<T>{
-    private Vector<T> vertices; //Vector to hold the vertices in the graph
-    private Vector<LinkedList<T>> arcs;   // Lists of adjacent vertices
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Vector;
+import javafoundations.Graph;
 
-   public HollywoodGraph(){
-    this.arcs = new Vector<LinkedList<T>>();
-    this.vertices = new Vector<T>();
-   }
+public class HollywoodGraph<T> implements Graph<T> {
+    private Vector<T> vertices; // Vector to hold the vertices in the graph
+    private Vector<LinkedList<T>> arcs; // Lists of adjacent vertices
 
     /**
-     * Creates and returns a new graph of Strings using the data found
-     * in the input tgf file.
-     * If the file does not exist, a message is printed.
-     *
-     * @param String The name of the tgf file to read the graph from
-     * @return AdjListsDiGraph<String> the graph of Strings created
+     * Default constructor
      */
-    public static HollywoodGraph<String> HollywoodGraph(String tgf_fName) {
-        //create an empty graph of Strings
-        HollywoodGraph<String> g = new HollywoodGraph<String>();
-
-        try{
-            Scanner scanner = new Scanner(new File(tgf_fName));
-
-            //read vertices
-            while (!scanner.next().equals("#")){ //discard special symbol (#)
-                String token = scanner.next(); //read next String
-                g.addVertex(token); //add it a vertex to the graph
-            }
-
-            //read arcs
-            while (scanner.hasNext()){
-                int from = scanner.nextInt();
-                int to = scanner.nextInt();
-                g.addArc(from-1, to-1); //uses helper method
-            }
-            scanner.close();
-        } catch (IOException ex) {
-            System.out.println(" ***ERROR***  " +  tgf_fName + " file could not be read. ");
-        }
-        return g;
+    public HollywoodGraph() {
+        this.arcs = new Vector<LinkedList<T>>();
+        this.vertices = new Vector<T>();
     }
 
-   
- }
+    public HollywoodGraph(String dataFileName) {
+        try {
+            Scanner scan = new Scanner(new File(dataFileName));
+            String line;
+            String[] info; // stores the array created after line gets split
+            scan.nextLine();// get rid of first line
+
+            Movie movie;
+
+            while (scan.hasNextLine()) {
+                line = scan.nextLine();
+                info = line.split(",");
+
+                movie = new Movie(info[0]);
+                this.addVertex(null);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Invalid file name");
+        }
+    }
+
+    public void generateTGF() {
+
+    }
+
+    /**
+     * Determines if this graph is empty or not based on the number of vertices it
+     * has
+     * 
+     * @return true if the graph is empty
+     *         false if not
+     */
+    @Override
+    public boolean isEmpty() {
+        return this.vertices.size() == 0;
+    }
+
+    /**
+     * Returns the number of vertices (movies and actors) in the graph
+     * 
+     * @return int number of vertices
+     */
+    @Override
+    public int getNumVertices() {
+        return vertices.size();
+    }
+
+    /**
+     * Returns the number of arcs (relationships between movies and actors in the
+     * graph)
+     * 
+     * @return int
+     */
+    @Override
+    public int getNumArcs() {
+
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getNumArcs'");
+    }
+
+    @Override
+    public boolean isArc(T vertex1, T vertex2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isArc'");
+    }
+
+    @Override
+    public boolean isEdge(T vertex1, T vertex2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEdge'");
+    }
+
+    @Override
+    public void addVertex(T vertex) {
+        throw new UnsupportedOperationException("Unimplemented method 'addVertex'");
+    }
+
+    @Override
+    public void removeVertex(T vertex) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeVertex'");
+    }
+
+    @Override
+    public void addArc(T vertex1, T vertex2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addArc'");
+    }
+
+    @Override
+    public void removeArc(T vertex1, T vertex2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeArc'");
+    }
+
+    @Override
+    public void addEdge(T vertex1, T vertex2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addEdge'");
+    }
+
+    @Override
+    public void removeEdge(T vertex1, T vertex2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeEdge'");
+    }
+
+    @Override
+    public void saveTGF(String tgf_file_name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'saveTGF'");
+    }
+
+}
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-### Gender Imbalance in a Movie Graph
+# Gender Imbalance in a Movie Graph
 ## CS230-01 Data Structures [final project](https://docs.google.com/document/d/1IycwGMGrk4UIMnbUNGa4i-qg65y6vre8VQmrD0hJ4Z8/edit)
 Authors: Sophie Lin, Rachel Hu, Lilymoon Whalen

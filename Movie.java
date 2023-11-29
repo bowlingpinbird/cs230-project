437ddcb47d57d098dde88da9fa19253e2371e7ca
sophieelin <56371476+sophieelin@users.noreply.github.com>
2023-11-28 20:17:47 -0500

    implemented movie

---
+++ Movie.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+/**
+ * @author Sophie Lin
+ * @author Rachel Hu
+ * @author Lilymoon Whalen
+ * @version 11/28/2023
+ */
+public class Movie{
+    private ArrayList<Actor> actors = new ArrayList<Actor>();
+    private String name;
+    public Movie(String movieName){
+        name = movieName;
+    }
+    public String getName(){
+        return name;
+    }
+}

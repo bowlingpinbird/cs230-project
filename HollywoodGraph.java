e46b2430713597a51aaf0e99ae159a455f7d3afb
rachel hu <rh111@wellesley.edu>
2023-11-30 19:44:35 -0500

    organized stuff

--- HollywoodGraph.java
+++ HollywoodGraph.java
@@ -23,16 +23,18 @@ import java.io.PrintWriter;
 import java.io.IOException;
 
 /**
-/**
+ * /**
+ * 
  * @author Sophie Lin
  * @author Rachel Hu
  * @author Lilymoon Whalen
  * @version 11/28/2023
  */
 
-public class HollywoodGraph<T> implements Graph<T>{
-    private Vector<T> vertices; //Vector to hold the vertices in the graph
-    private Vector<LinkedList<T>> arcs;   // Lists of adjacent vertices
+public class HollywoodGraph<T> implements Graph<T> {
+    private Vector<T> vertices; // Vector to hold the vertices in the graph
+    private Vector<LinkedList<T>> arcs; // Lists of adjacent vertices
+
     /**
      * Default constructor
      */
@@ -54,7 +56,6 @@ public class HollywoodGraph<T> implements Graph<T>{
                 line = scan.nextLine();
                 info = line.split(",");
 
-
                 movie = new Movie(info[0]);
                 this.addVertex(null);
             }
@@ -62,10 +63,6 @@ public class HollywoodGraph<T> implements Graph<T>{
         } catch (FileNotFoundException e) {
             System.out.println("Invalid file name");
         }
-    }
-
-    public void generateTGF() {
-
     }
 
     /**
@@ -95,9 +92,9 @@ public class HollywoodGraph<T> implements Graph<T>{
      * @return int
      */
     public int getNumArcs() {
-         int total = 0;
-        for(int i = 0; i < arcs.size(); i++){
-            total+= arcs.get(i).size(); 
+        int total = 0;
+        for (int i = 0; i < arcs.size(); i++) {
+            total += arcs.get(i).size();
         }
 
         return total;
@@ -105,25 +102,19 @@ public class HollywoodGraph<T> implements Graph<T>{
 
     public boolean isArc(T vertex1, T vertex2) {
         int s = vertices.indexOf(vertex1);
-        if(vertices.indexOf(vertex1)< 0 || vertices.indexOf(vertex2) < 0 ){
+        if (vertices.indexOf(vertex1) < 0 || vertices.indexOf(vertex2) < 0) {
             return false;
         } else {
             LinkedList<T> temp = new LinkedList<T>();
             temp = arcs.get(s);
-            for (int i = 0; i < temp.size(); i++){
-                if (temp.get(i).equals(vertex2)){
-                    return true;                  
+            for (int i = 0; i < temp.size(); i++) {
+                if (temp.get(i).equals(vertex2)) {
+                    return true;
                 }
             }
             return false;
 
-        }  
-    }
-
-    @Override
-    public boolean isEdge(T vertex1, T vertex2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isEdge'");
+        }
     }
 
     @Override
@@ -135,21 +126,23 @@ public class HollywoodGraph<T> implements Graph<T>{
      * Removes the input vertex from the graph.
      * If the input vertex does not belong in the graph, the graph is not changed.
      * Uses equals() for identidying the vertex to be removed.
+     * 
      * @param T The vertex to be removed.
      */
     @Override
-    public void removeVertex (T vertex) {
-        if (isVertex(vertex)) { //if the given vertex actually exists
-            //remove arcs pointing to the vertex
-            for (int vert = 0; vert < this.arcs.size(); vert ++) { //iterate through arcs first layer, representing verticies
+    public void removeVertex(T vertex) {
+        if (isVertex(vertex)) { // if the given vertex actually exists
+            // remove arcs pointing to the vertex
+            for (int vert = 0; vert < this.arcs.size(); vert++) { // iterate through arcs first layer, representing
+                                                                  // verticies
                 int keepNumArcs = this.arcs.elementAt(vert).size();
-                for (int arc = 0; arc < keepNumArcs; arc ++) { //iterate through each vertex's linked list of arcs
+                for (int arc = 0; arc < keepNumArcs; arc++) { // iterate through each vertex's linked list of arcs
                     if (this.arcs.elementAt(vert).get(arc).equals(vertex)) {
-                        removeArc(this.vertices.elementAt(vert), vertex); 
+                        removeArc(this.vertices.elementAt(vert), vertex);
                     }
                 }
             }
-            //remove the vertex
+            // remove the vertex
             int targetVertIndex = this.vertices.indexOf(vertex);
             this.vertices.remove(vertex);
             this.arcs.remove(targetVertIndex);
@@ -159,37 +152,48 @@ public class HollywoodGraph<T> implements Graph<T>{
     @Override
     public void addArc(T vertex1, T vertex2) {
         int s = vertices.indexOf(vertex1);
-        if(vertices.indexOf(vertex1)< 0 || vertices.indexOf(vertex2) < 0 ){
+        if (vertices.indexOf(vertex1) < 0 || vertices.indexOf(vertex2) < 0) {
 
         } else {
             LinkedList<T> temp = new LinkedList<T>();
             temp = arcs.get(s);
             boolean shouldAdd = true;
-            for (int i = 0; i < temp.size(); i++){
-                if (temp.get(i).equals(vertex2)){
-                    shouldAdd = false;                    
+            for (int i = 0; i < temp.size(); i++) {
+                if (temp.get(i).equals(vertex2)) {
+                    shouldAdd = false;
                 }
 
             }
-            if(shouldAdd){
+            if (shouldAdd) {
                 arcs.get(s).add(vertex2);
             }
         }
     }
 
     public void removeArc(T vertex1, T vertex2) {
-        int s=vertices.indexOf(vertex1);
-        if(vertices.indexOf(vertex1)< 0 || vertices.indexOf(vertex2) < 0 ){
+        int s = vertices.indexOf(vertex1);
+        if (vertices.indexOf(vertex1) < 0 || vertices.indexOf(vertex2) < 0) {
         } else {
             LinkedList<T> temp = new LinkedList<T>();
             temp = arcs.get(s);
-            for (int i = 0; i < temp.size(); i++){
-                if (temp.get(i).equals(vertex2)){
+            for (int i = 0; i < temp.size(); i++) {
+                if (temp.get(i).equals(vertex2)) {
                     temp.remove(s);
                 }
             }
 
         }
+    }
+
+    /**
+     * 
+     * @param vertex1
+     * @param vertex2
+     * @return
+     */
+    @Override
+    public boolean isEdge(T vertex1, T vertex2) {
+        return isArc(vertex1, vertex2) && isArc(vertex2, vertex1);
     }
 
     @Override
@@ -200,19 +204,19 @@ public class HollywoodGraph<T> implements Graph<T>{
 
     @Override
     public void removeEdge(T vertex1, T vertex2) {
-        
+
     }
 
     /**
      * Checks if the given vertex exists in this graph
+     * 
      * @param vertex to find
      * @return true if the vertex exists
      *         false if not
      */
-    public boolean isVertex (T vertex) {
+    public boolean isVertex(T vertex) {
         return (this.vertices.indexOf(vertex) != -1);
     }
-
 
     @Override
     public void saveTGF(String tgf_file_name) {

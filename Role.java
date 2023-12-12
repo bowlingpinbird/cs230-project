b406c9813eb3e894409d284f7f523f35aa0e3e40
Lily <lilymoonsun@outlook.com>
2023-12-12 17:09:22 -0500

    added comments to role class

--- Role.java
+++ Role.java
@@ -1,5 +1,6 @@
 /**
- * Represents an actor's Role in a movie, with the name of the role, type, billing, and gender
+ * Represents an actor's Role in a movie, with the name of the role, type,
+ * billing, and gender
  * 
  * @author Lilymoon Whalen
  * @author Sophie Lin
@@ -9,11 +10,20 @@
  */
 
 public class Role {
-    private String gender;
+    private String gender; // gender of the role the actor plays
     private String roleName;
     private String roleType;
     private String billing;
 
+    /**
+     * creates a role does not include movie name since it is included in the
+     * Dictionary in actor
+     * 
+     * @param roleName
+     * @param roleTypeName
+     * @param billing
+     * @param genderName
+     */
     public Role(String roleName, String roleTypeName, String billing, String genderName) {
         gender = genderName;
         this.billing = billing;
@@ -21,14 +31,23 @@ public class Role {
         roleType = roleTypeName;
     }
 
+    /**
+     * @return gender of the role actor plays
+     */
     public String getGender() {
         return gender;
     }
 
+    /**
+     * @return name of the character actor plays
+     */
     public String getRoleName() {
         return roleName;
     }
 
+    /**
+     * @return the role type actor plays, e.g. Leading Supporting
+     */
     public String getRoleType() {
         return roleType;
     }
@@ -37,8 +56,12 @@ public class Role {
         return billing;
     }
 
+    /**
+     * string representation of role actor plays
+     */
     public String toString() {
-        return  "Role: " + this.roleName + ", Gender: " + this.gender + ", Role type: " + this.roleType + ", Billing: " + this.billing + "\n";
+        return "Role: " + this.roleName + ", Gender: " + this.gender + ", Role type: " + this.roleType + ", Billing: "
+                + this.billing + "\n";
     }
 
 }
